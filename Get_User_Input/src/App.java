ba2d72ac8c1db8ee501395a6c2931d4c655f9a9f
Jay Manhire <jaymanhire@gmail.com>
2018-08-25 21:12:44 -0400

    Java JVM test

---
+++ Get_User_Input/src/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a line of text: ");	
+		
+		String line = input.nextLine();
+		
+		System.out.println("You entered: " + line);
+		
+		//input.close();
+		
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Enter floating point value: ");	
+		
+		float floatVal = input2.nextFloat();
+		
+		System.out.println("You entered: " + floatVal);
+			
+		input.close();
+		input2.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

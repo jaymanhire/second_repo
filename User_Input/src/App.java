f3a961ec28ec552c1d3bc9fbef1821242d37e439
Jay Manhire <jaymanhire@gmail.com>
2018-08-25 21:40:56 -0400

    Test

--- User_Input/src/App.java
+++ User_Input/src/App.java
@@ -2,34 +2,26 @@ import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a line of text: ");	
-		
+		System.out.println("Enter a line of text: ");
+
 		String line = input.nextLine();
-		
+
 		System.out.println("You entered: " + line);
-		
-		//input.close();
-		
-		Scanner input2 = new Scanner(System.in);  
-		System.out.println("Enter floating point value: ");	
-		
+
+		// input.close();
+
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Enter floating point value: ");
+
 		float floatVal = input2.nextFloat();
-		
+
 		System.out.println("You entered: " + floatVal);
-			
+
 		input.close();
 		input2.close();
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }

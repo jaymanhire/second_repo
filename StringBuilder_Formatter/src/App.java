85cdf4e675aa66ffbeceddcb84c4d1aa2b32e7e3
Jay Manhire <jaymanhire@gmail.com>
2018-10-02 20:15:58 -0400

    String stuff

---
+++ StringBuilder_Formatter/src/App.java
@@ -0,0 +1,42 @@
+
+public class App {
+
+	public static void main(String[]args) {
+		
+		String info = "";
+		
+		info += "My name is Bob.";
+		info += " ";
+		info += "I am a builder.";
+		
+		System.out.println(info);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("My name is Sue.");
+		sb.append(" ");
+		sb.append("I am a lion tamer.");
+		
+		System.out.println(sb);
+		
+		StringBuilder s = new StringBuilder();
+		s.append("My name is Roger.")
+		.append(" ")
+		.append("I am a skydiver.");
+		System.out.println(s.toString());
+		
+		System.out.print("Here is some text.\tThat was a tab.\nThat was a new line.\n");
+		
+		System.out.printf("Total cost %10d; quantity: %d", 5, 120);
+		
+		for(int i = 0; i < 15; i++) {
+			System.out.printf("%-2d: Some text\n", i);
+		}
+		
+		System.out.printf("Total Value: %12.3f", 9879.321585);
+		
+		
+		
+		
+	}
+
+}

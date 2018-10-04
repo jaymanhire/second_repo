f72bfcc9e2ff813ccf3ec86812e2583c366c814f
Jay Manhire <jaymanhire@gmail.com>
2018-10-04 14:57:28 -0400

    Swap a for b

---
+++ Variable_Swap/src/App.java
@@ -0,0 +1,18 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 29;
+
+		int swap = a;
+		a = b;
+		b = swap;
+
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+
+	}
+
+}

606998527a1906a1b0dbc1b073def48bd14a70d6
Jay Manhire <jaymanhire@gmail.com>
2018-10-11 17:06:52 -0400

    Exceptions

---
+++ Handling_Exceptions/src/demo3/App.java
@@ -0,0 +1,25 @@
+package demo3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class App {
+
+	public static void main(String[] args) {
+		try {
+			openFile();
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot find file.");
+		}
+
+	}
+
+	public static void openFile() throws FileNotFoundException {
+
+		File file = new File("test.txt");
+
+		FileReader fr = new FileReader(file);
+	}
+
+}

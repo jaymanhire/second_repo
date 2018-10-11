d031bcbb0d45a3608e1eee275f1b418dcad2fcf3
Jay Manhire <jaymanhire@gmail.com>
2018-10-11 00:52:44 -0400

    Additional Formatting

--- ReadFile/src/App.java
+++ ReadFile/src/App.java
@@ -6,20 +6,34 @@ public class App {
 
 	public static void main(String[] args) {
 
-		String fileName = "/home/jlm/Documents/test.txt";
+		//String fileName = "/home/jlm/Documents/test.txt";
+		String fileName = "test.txt";
 
 		File textFile = new File(fileName);
 
 		try {
 			Scanner in = new Scanner(textFile);
+			
+			int value = in.nextInt();
+			System.out.println("Read value: " + value);
+			
+			in.nextLine();  // this reads my new line character, leaves out blank line
+			
+			int count = 2;
+			
+			
 			while (in.hasNextLine()) {
 				String line = in.nextLine();
-				System.out.println(line);
+				
+				System.out.println(count + ": " + line);
+				count++;
 			}
+			
 			in.close();
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("Cannot open file: " + fileName);
-			//e.printStackTrace();
+			
 		}
 
 	}

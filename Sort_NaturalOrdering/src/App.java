948246e3136cf7237d243cc92bd51eb8ac9f3dd3
Jay Manhire <jaymanhire@gmail.com>
2018-10-18 19:29:47 -0400

    Part 2

--- Sort_NaturalOrdering/src/App.java
+++ Sort_NaturalOrdering/src/App.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-/*class Person {
+class Person implements Comparable<Person> {
 	private String name;
 
 	public Person(String name) {
@@ -14,15 +14,51 @@ import java.util.TreeSet;
 
 	public String toString() {
 		return name;
+	}	
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
-}*/
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Person person) {		
+		return -name.compareTo(person.name);
+	}
+}
 
 public class App {
 
 	public static void main(String[] args) {
 
-		List<String> list = new ArrayList<String>();
-		SortedSet<String> set = new TreeSet<String>(); // TreeSets Sorted automatically
+		List<Person> list = new ArrayList<Person>();
+		SortedSet<Person> set = new TreeSet<Person>(); // TreeSets Sorted automatically
 
 		addElements(list);
 		addElements(set);
@@ -35,16 +71,16 @@ public class App {
 
 	}
 
-	private static void addElements(Collection<String> col) {
-		col.add("Joe");
-		col.add("Sue");
-		col.add("Juliet");
-		col.add("Clare");
-		col.add("Mike");
+	private static void addElements(Collection<Person> col) {
+		col.add(new Person ("Joe"));
+		col.add(new Person ("Sue"));
+		col.add(new Person ("Juliet"));
+		col.add(new Person ("Clare"));
+		col.add(new Person ("Mike"));
 	}
 
-	private static void showElements(Collection<String> col) {
-		for (String element : col) {
+	private static void showElements(Collection<Person> col) {
+		for (Person element : col) {
 			System.out.println(element);
 		}
 	}

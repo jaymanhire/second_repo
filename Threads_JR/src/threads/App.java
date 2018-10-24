61f5327119ddd16b87dfac4226216e3b405a7fcd
Jay Manhire <jaymanhire@gmail.com>
2018-10-23 21:34:14 -0400

    implements Runnable

--- Threads_JR/src/threads/App.java
+++ Threads_JR/src/threads/App.java
@@ -1,20 +1,30 @@
-
+package threads;
 public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("Starting thread 1: ");
+		/*System.out.println("Starting thread 1: ");
 		Task taskRunner = new Task("Thread_1");
-		taskRunner.start();
+		Thread t1 = new Thread(taskRunner);
+		t1.start();
 
 		System.out.println("Starting thread 2: ");
 
 		Task taskRunner2 = new Task("Thread_2");
-		taskRunner2.start();
+		Thread t2 = new Thread(taskRunner2);
+		t2.start();*/
+		// OR --
+		System.out.println("Starting thread 1: ");		
+		Thread t1 = new Thread(new Task("Thread_1"));
+		t1.start();
+
+		System.out.println("Starting thread 2: ");		
+		Thread t2 = new Thread(new Task("Thread_2"));
+		t2.start();
 	}
 }
 
-class Task extends Thread {
+class Task implements Runnable {
 
 	String name;
 
@@ -26,7 +36,7 @@ class Task extends Thread {
 
 		Thread.currentThread().setName(name);
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
 			System.out.println("Index: " + i + ": " + Thread.currentThread().getName());
 			try {
 				Thread.sleep(10);

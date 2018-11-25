358844368c2f3ef6bd03a899fedbd412380ef0d5
Jay Manhire <jaymanhire@gmail.com>
2018-11-24 19:20:31 -0500

    char replace method

---
+++ Java_Hodge/src/StringReplace.java
@@ -0,0 +1,12 @@
+
+public class StringReplace {
+
+	public static void main(String[] args) {
+		
+		String s = "dog";
+		String replaced_f = s.replace('o', 'i');
+		System.out.println(replaced_f);
+
+	}
+
+}

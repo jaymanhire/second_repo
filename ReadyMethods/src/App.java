bfea4e572bcda61d0450db6227426bcce7cdaf2b
Jay Manhire <jaymanhire@gmail.com>
2018-12-14 23:21:43 -0500

    Basic Static methods

---
+++ ReadyMethods/src/App.java
@@ -0,0 +1,22 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("wsfwuysg");	
+		
+		
+		int aa = MyUtils.printSomeJunk(12348);
+		System.out.println(aa);
+		
+		int a = MyUtils.add10(981321);
+		System.out.println(a); 
+		
+
+	}
+	
+	
+	
+	
+
+}
---
+++ ReadyMethods/src/MyUtils.java
@@ -0,0 +1,13 @@
+
+public class MyUtils {
+	
+	public static int add10(int myNum) {
+		int result = myNum + 10;
+		return result;
+	}
+	public static int printSomeJunk(int number) {
+		int result = number;
+		return result;
+	}
+
+}

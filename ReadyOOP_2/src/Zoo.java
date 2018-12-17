7bc835274788b9e7d985baaae8455f1c7db747f7
Jay Manhire <jaymanhire@gmail.com>
2018-12-17 11:39:04 -0500

    Final Version

--- ReadyOOP_2/src/Zoo.java
+++ ReadyOOP_2/src/Zoo.java
@@ -1,25 +1,13 @@
 
 public class Zoo {
 	public static void main(String[] args) {
-
-		/*
-		 * Animal sparrow1 = new Sparrow(1, 2, "M", "Sparrow"); sparrow1.move();
-		 */
-
-		Sparrow sparrow1 = new Sparrow(1, 2, "M", "Sparrow");
-		//sparrow1.move();
-		//sparrow1.fly();
-
-		Animal fish1 = new Fish();
-		//fish1.move();
 		
-		moveAnimals(fish1);
-		moveAnimals(sparrow1);
+		Flyable flyingBird1 = new Sparrow(1, 3, "M", "sparrow");
+		flyingBird1.fly();
+		
+		/*Flyable flyingBird2 = new Bird(1, 3, "M", "bird");
+		flyingBird2.fly();*/
+		
 
 	}
-	
-	public static void moveAnimals(Animal animal) {
-		animal.move();
-	}
-
 }

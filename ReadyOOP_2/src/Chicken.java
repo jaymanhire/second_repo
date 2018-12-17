5239c7092468da1e309818eec9faf8fa08cca82f
Jay Manhire <jaymanhire@gmail.com>
2018-12-17 11:17:43 -0500

    Interface Correction

--- ReadyOOP_2/src/Chicken.java
+++ ReadyOOP_2/src/Chicken.java
@@ -1,9 +1,15 @@
 
-public class Chicken extends Bird {
+public class Chicken extends Bird implements Flyable {
 
 	public Chicken(int age, int weight, String gender, String animalType) {
 		super(age, weight, gender, animalType);
 
 	}
+
+	@Override
+	public void fly() {
+		System.out.println("Chicken unable to fly.");
+	}
+	
 
 }

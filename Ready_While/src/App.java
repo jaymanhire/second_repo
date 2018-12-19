6a7cc8947cbe5467cebcf7a644775d7a1fbe5ea1
Jay Manhire <jaymanhire@gmail.com>
2018-12-18 20:00:17 -0500

    Find instances of a String

---
+++ Ready_While/src/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+	public static void main(String[] args) {
+
+		int count = 0;
+
+		while (count < 10) { 
+			System.out.println("Jay: " + count);
+			count++;
+			if(count == 6) {
+				break;
+			}
+			
+			//continue;
+		}
+		
+		
+		
+
+	}
+}

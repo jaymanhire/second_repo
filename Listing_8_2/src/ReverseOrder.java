e0f968c4bd227822fa5142a7ee3d6eec9b1031a2
Jay Manhire <jaymanhire@gmail.com>
2018-12-19 20:54:13 -0500

    Take input-Store in array

---
+++ Listing_8_2/src/ReverseOrder.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ReverseOrder {
+	public static void main(String[] args) {
+		// System.out.println();
+
+		Scanner scan = new Scanner(System.in);
+
+		double[] numbers = new double[5];
+
+		System.out.println("The size of the array: " + numbers.length);
+
+		for (int index = 0; index < numbers.length; index++) {
+			System.out.println("Enter number " + (index + 1) + ": ");
+			numbers[index] = scan.nextDouble();
+		}
+
+		System.out.println("The numbers in reverse order: ");
+
+		for (int index = numbers.length - 1; index >= 0; index--) {
+			System.out.println("Number " + (index + 1) + ": " + numbers[index] + " ");
+		}
+		scan.close();
+	}
+
+}

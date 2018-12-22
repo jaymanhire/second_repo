ef03a1279315a6e649071254a24a9ffbf563de07
Jay Manhire <jaymanhire@gmail.com>
2018-12-21 21:37:39 -0500

    Comment/Uncomment the hashcode method to generate. Compare custom objects

--- ArrayList_LinkedList/src/com/hashset/linkedhashset/App.java
+++ ArrayList_LinkedList/src/com/hashset/linkedhashset/App.java
@@ -1,21 +1,33 @@
 package com.hashset.linkedhashset;
 
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 
 public class App {
 
 	public static void main(String[] args) {
 		//Prevents duplicate values		
 		//HashSet doe not maintain order, LinkedHashSet maintains entry order
-		LinkedHashSet<String> values = new LinkedHashSet<>();
-		values.add("right");
-		values.add("car");
-		values.add("zoo");
-		values.add("zoo");
-		values.add("car");
+		//User defined objects: unable to compare accurately
+		HashSet<Animal> animals = new HashSet<Animal>();
 		
 		
-		for(String value: values) {
+		Animal animal1 = new Animal("Dog", 12);
+		Animal animal2= new Animal("Cat", 8);
+		Animal animal3 = new Animal("Bird", 3);
+		Animal animal4 = new Animal("Dog", 12);
+		Animal animal5 = new Animal("Kangaroo", 24);
+		
+		animals.add(animal1);
+		animals.add(animal2);
+		animals.add(animal3);
+		animals.add(animal4);
+		animals.add(animal5);
+		
+		System.out.println(animal1.equals(animal4)); // Yields false if not using hash-equals
+		System.out.println(animal1.hashCode());
+		System.out.println(animal4.hashCode());
+		
+		for(Animal value: animals) {
 			System.out.println(value);
 		}
 		

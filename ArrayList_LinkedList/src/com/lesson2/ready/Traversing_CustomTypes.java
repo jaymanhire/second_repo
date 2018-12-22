6750d4be364967671c7f561b362dfe838bf83bbc
Jay Manhire <jaymanhire@gmail.com>
2018-12-21 19:26:08 -0500

    ArrayList can be changed to Linked then to List. toString.

--- ArrayList_LinkedList/src/com/lesson2/ready/Traversing_CustomTypes.java
+++ ArrayList_LinkedList/src/com/lesson2/ready/Traversing_CustomTypes.java
@@ -1,12 +1,14 @@
 package com.lesson2.ready;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Traversing_CustomTypes {
 
 	public static void main(String[] args) {
 
-		ArrayList<String> animals = new ArrayList<String>();
+		List<String> animals = new ArrayList<String>();
 
 		animals.add("Lion");
 		animals.add("Cat");
@@ -20,23 +22,35 @@ public class Traversing_CustomTypes {
 		 * for (String anims : animals) { System.out.println(anims); }
 		 */
 
-		ArrayList<Vehicle> vehicles = new ArrayList<>(10);
+		List<Vehicle> vehicles = new LinkedList<Vehicle>();
 
-		/*Vehicle vehicle2 = new Vehicle("Honda", "Accord", 12000, false);	// Option 1
-		vehicles.add(vehicle2);*/
-		
-		vehicles.add(new Vehicle("Honda", "Accord", 12000, false));		//Option 2
+		/*
+		 * Vehicle vehicle2 = new Vehicle("Honda", "Accord", 12000, false); // Option 1
+		 * vehicles.add(vehicle2);
+		 */
+
+		vehicles.add(new Vehicle("Honda", "Accord", 12000, false)); // Option 2
 		vehicles.add(new Vehicle("Jeep", "Wrangler", 25000, true));
 		vehicles.add(new Vehicle("Chevy", "Malibu", 16500, false));
-		
-		for(Vehicle car: vehicles) {
+
+		for (Vehicle car : vehicles) {
 			System.out.println(car.toString());
 		}
 
-		/*for (Vehicle car : vehicles) {
-			System.out.println(car.getMake());
-			System.out.println(car.getModel());
-		}*/
+		/*
+		 * for (Vehicle car : vehicles) { System.out.println(car.getMake());
+		 * System.out.println(car.getModel()); }
+		 */
+		printElements(vehicles);
+		printElements(animals);
+		
+	}
+
+	public static void printElements(List<?> someList) {
+		for (int i = 0; i < someList.size(); i++) {
+			System.out.println(someList.get(i));
+			//someList.remove(0);
+		}
 
 	}
 }

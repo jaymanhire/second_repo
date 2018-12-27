426ee93228e638fba7cf48a63ca3912e8e813075
Jay Manhire <jaymanhire@gmail.com>
2018-12-27 18:27:00 -0500

    Implements Runnable shorter code version 2

--- MultiThreading_Ready_36/src/Application.java
+++ MultiThreading_Ready_36/src/Application.java
@@ -4,14 +4,12 @@ public class Application {
 	public static void main(String[] args) {
 
 		System.out.println("Starting Thread 1...");
-		Task taskRunner = new Task("Thread-A");
-		Thread t1 = new Thread(taskRunner);
+		Thread t1 = new Thread(new Task("Thread-A"));
 		t1.start();
 
-		System.out.println("Starting Thread 2...");		
-		Task taskRunner2 = new Task("Thread-B");
-		Thread t2 = new Thread(taskRunner2);
-		t2.start(); 
+		System.out.println("Starting Thread 2...");
+		Thread t2 = new Thread(new Task("Thread-B"));
+		t2.start();
 
 	}
 
@@ -27,7 +25,7 @@ class Task implements Runnable {
 
 	public void run() {
 		Thread.currentThread().setName(name);
-		
+
 		for (int i = 0; i < 1000; i++) {
 			System.out.println("number: " + i + " - " + Thread.currentThread().getName());
 			try {

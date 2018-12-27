aba495b87d87c3f9b9010b8168b8ef1e4913d4fd
Jay Manhire <jaymanhire@gmail.com>
2018-12-27 18:33:58 -0500

    Runnable with Anonymous class defined to Constructor of the Thread class

--- MultiThreading_Ready_36/src/Application.java
+++ MultiThreading_Ready_36/src/Application.java
@@ -4,11 +4,43 @@ public class Application {
 	public static void main(String[] args) {
 
 		System.out.println("Starting Thread 1...");
-		Thread t1 = new Thread(new Task("Thread-A"));
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				for (int i = 0; i < 1000; i++) {
+					System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+		});
 		t1.start();
 
 		System.out.println("Starting Thread 2...");
-		Thread t2 = new Thread(new Task("Thread-B"));
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				for (int i = 0; i < 1000; i++) {
+					System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+		});
 		t2.start();
 
 	}
@@ -24,8 +56,6 @@ class Task implements Runnable {
 	}
 
 	public void run() {
-		Thread.currentThread().setName(name);
-
 		for (int i = 0; i < 1000; i++) {
 			System.out.println("number: " + i + " - " + Thread.currentThread().getName());
 			try {

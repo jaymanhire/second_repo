3d95a42e5b801f44993ea7b7a94fb273496383d4
Jay Manhire <jaymanhire@gmail.com>
2018-12-30 01:38:54 -0500

    Wait and Notify

---
+++ MultiThreading_Cave/src/wait/notify/Processor.java
@@ -0,0 +1,30 @@
+package wait.notify;
+
+import java.util.Scanner;
+
+public class Processor {
+	
+	public void produce() throws InterruptedException {
+		synchronized (this) {
+			System.out.println("Producer thread running...");
+			wait();  // only call in sync. code blocks
+			System.out.println("Resumed.");
+		}
+		
+	}
+	public void consume() throws InterruptedException {
+		Scanner scan = new Scanner(System.in);
+		Thread.sleep(2000);
+		
+		synchronized (this) {
+			System.out.println("Waiting for return key.");
+			scan.nextLine();
+			System.out.println("Return key pressed.");
+			notify();
+			Thread.sleep(5000);
+			scan.close();
+			
+		}
+		
+	}
+}

63987e9c114e6ca9408881b09ac0c7be1afe15d3
Jay Manhire <jaymanhire@gmail.com>
2018-12-31 13:43:48 -0500

    Producer-Consumer Design Pattern

---
+++ MultiThreading_Ready_39/src/com/jlm/producerconsumer/Consumer.java
@@ -0,0 +1,38 @@
+package com.jlm.producerconsumer;
+
+import java.util.List;
+
+public class Consumer implements Runnable {
+
+	List<Integer> questionList = null;
+
+	public Consumer(List<Integer> questionList) {
+		this.questionList = questionList;
+	}
+
+	public void answerQuestion() throws InterruptedException {
+		synchronized (questionList) {
+			while (questionList.isEmpty()) {
+				System.out.println("No Questions to answer...Waiting for producer for questions.");
+				questionList.wait();
+			}
+		}
+		synchronized (questionList) {
+			Thread.sleep(300);
+			System.out.println("Answered question: " + questionList.remove(0));
+			questionList.notify();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				answerQuestion();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}

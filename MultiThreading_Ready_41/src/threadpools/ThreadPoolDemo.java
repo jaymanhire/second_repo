bf1597e8210dc0c46b747e461d664df5f74c6c3d
Jay Manhire <jaymanhire@gmail.com>
2018-12-31 23:25:04 -0500

    Thread Pools

---
+++ MultiThreading_Ready_41/src/threadpools/ThreadPoolDemo.java
@@ -0,0 +1,44 @@
+package threadpools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolDemo {
+	
+	public static void main(String[] args) {
+		
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		
+		Runnable processor = new MessageProcessor(1);
+		executor.execute(processor);
+		
+		Runnable processor2 = new MessageProcessor(2);
+		executor.execute(processor2);
+		
+		Runnable processor3 = new MessageProcessor(3);
+		executor.execute(processor3);
+		
+		Runnable processor4 = new MessageProcessor(4);
+		executor.execute(processor4);
+		
+		executor.shutdown();  // rejects new submissions of tasks
+		//executor.shutdownNow(); // Terminte the executor service immediately.
+		try {
+			executor.awaitTermination(10, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		/*while(!executor.isTerminated()) {
+			//System.out.println();
+		}*/
+		
+		System.out.println("Submitted all tasks...");
+		
+		
+		
+		
+	}
+
+}

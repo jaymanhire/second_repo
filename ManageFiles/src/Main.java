424c76c42b1ff17d407f3dfb59aa09c9095a7135
Jay Manhire <jaymanhire@gmail.com>
2019-02-13 17:06:18 -0500

    Add, create, delete files and directories

---
+++ ManageFiles/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		Path source = Paths.get("files/loremipsum.txt");
+		System.out.println(source.getFileName());
+		
+		Path target = Paths.get("files/newfile.txt");
+		Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+		
+		/*// Delete a file(To test, first copy and paste one of the files in the project and rename "toDelete")
+		Path toDelete = Paths.get("files/toDelete.txt");		
+		Files.delete(toDelete);
+		System.out.println("File deleted");*/
+		
+		Path newdir = Paths.get("files/newdir");
+		Files.createDirectories(newdir);
+		
+		Files.move(source, newdir.resolve(source.getFileName()),
+		StandardCopyOption.REPLACE_EXISTING);
+		
+	}
+
+}

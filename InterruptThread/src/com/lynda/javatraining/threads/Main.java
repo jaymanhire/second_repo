4577e6e72daef598cf62ad799b16d59cf44ac2e0
Jay Manhire <jaymanhire@gmail.com>
2019-02-14 16:17:53 -0500

    Interrupting Threads

---
+++ InterruptThread/src/com/lynda/javatraining/threads/Main.java
@@ -0,0 +1,20 @@
+package com.lynda.javatraining.threads;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		MyThread t = new MyThread();
+		t.start();
+
+		try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		t.interrupt();
+		System.out.println("Called Interrupt();");
+	}
+
+}

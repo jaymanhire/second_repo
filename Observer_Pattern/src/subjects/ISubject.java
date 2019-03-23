802b7402e488cdc4403837b00c9a73219a6b27eb
Jay Manhire <jaymanhire@gmail.com>
2019-03-23 11:02:41 -0400

    Observer Pattern

---
+++ Observer_Pattern/src/subjects/ISubject.java
@@ -0,0 +1,12 @@
+package subjects;
+
+import observers.IObserver;
+
+public interface ISubject {
+	
+	public void registerObserver(IObserver o);
+	public void removeObserver(IObserver o);
+	public void notifyObserver();
+	
+
+}

3ed97a0928e82af3e01dbeb25b60c95acd7e042a
Jay Manhire <jaymanhire@gmail.com>
2019-03-26 11:42:02 -0400

    Singleton Design Pattern

---
+++ Singleton_Pattern/src/Application.java
@@ -0,0 +1,21 @@
+
+public class Application {
+	public static void main(String[] args) {
+
+		PerformanceStage stage = PerformanceStage.getInstance();
+		
+		/*PerformanceStage stage2 = new PerformanceStage();
+		PerformanceStage stage3 = new PerformanceStage();
+		PerformanceStage stage4 = new PerformanceStage();
+		PerformanceStage stage5 = new PerformanceStage();*/
+		
+		//System.out.println(stage.getCounter());
+		
+
+		stage.turnOnLights();
+		System.out.println(stage.getCounter());
+
+		
+
+	}
+}
---
+++ Singleton_Pattern/src/PerformanceStage.java
@@ -0,0 +1,29 @@
+
+public class PerformanceStage {
+
+	private static PerformanceStage INSTANCE = null;
+	
+	private static int counter;
+
+	// no public constructor in a Singleton
+	private PerformanceStage() {
+		counter++;
+	}
+
+	public synchronized static PerformanceStage getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new PerformanceStage();
+		}
+		return INSTANCE;
+	}
+	
+	public void turnOnLights() {
+		System.out.println("turned on lights...");
+	}
+	
+	public int getCounter() {
+		return counter;
+		
+	}
+
+}
